9be42197fe3666a977e2e088c06e71481e3bd821
adelsonc13-Costa <adelsonc13@gmail.com>
2026-09-02 12:17:25 -0300

    Update env.ts

--- server/_core/env.ts
+++ server/_core/env.ts
@@ -7,4 +7,6 @@ export const ENV = {
   isProduction: process.env.NODE_ENV === "production",
   forgeApiUrl: process.env.BUILT_IN_FORGE_API_URL ?? "",
   forgeApiKey: process.env.BUILT_IN_FORGE_API_KEY ?? "",
+  appUsername: process.env.APP_USERNAME ?? "",
+  appPassword: process.env.APP_PASSWORD ?? "",
 };
